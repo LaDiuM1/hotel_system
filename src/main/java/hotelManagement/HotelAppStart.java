6f9a3adb1fda69054e892e6d877454988210b977
kimsundae <naa159@naver.com>
2023-10-27 17:48:22 +0900

    Initial commit

---
+++ src/main/java/hotelManagement/HotelAppStart.java
@@ -0,0 +1,11 @@
+package hotelManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelAppStart {
+    public static void main(String[] args) {
+        SpringApplication.run(HotelAppStart.class);
+    }
+}
