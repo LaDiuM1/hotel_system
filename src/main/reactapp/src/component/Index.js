126ba01917d4a358555180b87c3822e5e272b5c1
z1s5c3x2 <z1s5c3x2@gmail.com>
2023-10-30 14:50:21 +0900

    Index 틀 구성

--- src/main/reactapp/src/component/Index.js
+++ src/main/reactapp/src/component/Index.js
@@ -1,33 +1,39 @@
 // 리액트 라우터 라이브러리
 import{ BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-
+import styles from './css/index.css'
 import Header from './Header'
-import SideCategory from './SideCategory'
+import Sidebar from './Sidebar'
+import Main from "./Main";
 
 
 
 
 
-function Index(props){
+export default function Index(props){
     return(<>
         <div className={'webContainer'}>
             <BrowserRouter>
-                <Header/>
-                <div className={}>
-                    <SideCategory/>
+                <div className={"sidebar"}>
+                    <Sidebar/>
+                </div>
+                <div className={"main"}>
+                    <div className={"header"}>
+                        <Header/>
+                    </div>
+                    <div className={"content"}>
+                        <Main/>
+                    </div>
+
+                </div>
+
                     <Routes>
 
 
                     </Routes>
-                </div>
             </BrowserRouter>
         </div>
     </>)
 }
-function Login(){
-    return(<>
 
-    </>)
-}
 
 
---
+++ src/main/reactapp/src/component/Sidebar.js
@@ -0,0 +1,15 @@
+/* 사이드바 컴포넌트 */
+import styles from './css/sidebar.css'
+
+export default function Sidebar(){
+
+    return(<>
+            <div className={"sidebar"}>
+
+                <h1> 사이드바 메뉴 </h1>
+
+
+            </div>
+        </>)
+
+}
