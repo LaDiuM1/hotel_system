5c3701fcc7d906d7c0f3f80fbd61bcdf2368f760
kimsundae <naa159@naver.com>
2023-10-30 14:50:33 +0900

    14:50 김근배 login 페이지 작성

--- src/main/reactapp/src/component/Index.js
+++ src/main/reactapp/src/component/Index.js
@@ -3,31 +3,53 @@ import{ BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 import Header from './Header'
 import SideCategory from './SideCategory'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import styles from './css/Login.css'
 
-
-
-
+let page = Login;
 
 function Index(props){
     return(<>
         <div className={'webContainer'}>
-            <BrowserRouter>
+{/*            <BrowserRouter>
                 <Header/>
                 <div className={}>
                     <SideCategory/>
                     <Routes>
-
+<div>
+    <div>
+      <SideCategory/>
+    </div>
+    <div>
+         <Header/>
+        <Routes>
+    </div>
+</div>
 
                     </Routes>
                 </div>
-            </BrowserRouter>
+            </BrowserRouter> */}
         </div>
     </>)
 }
+
 function Login(){
     return(<>
-
+        <div>
+            <div className={"loginWrap"}>
+                <div>
+                    <h1>Hotel Management</h1>
+                    <input type={"text"} placeholder={"사원번호"}></input><br/>
+                    <input type={"password"} placeholder={"비밀번호"}></input>
+                </div>
+                <div className={"loginBtnBox"}>
+                    <button type={"button"} className={"loginBtn"}>login</button>
+                </div>
+            </div>
+        </div>
     </>)
 }
-
+export default page;
 
