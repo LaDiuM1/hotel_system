94af7ce6c00d6082de8afda56b0e2b15b87fef0b
kimsundae <naa159@naver.com>
2023-10-31 17:01:01 +0900

    17:00 사이드바 수정

--- src/main/reactapp/src/component/Sidebar.js
+++ src/main/reactapp/src/component/Sidebar.js
@@ -13,12 +13,12 @@ export default function Sidebar(){
             <div className={"sidebar"}>
                 <div className={"categoryMenu"}>
                     <div className={"mainText"}> <Link to={"/"} onClick={(e)=>{changeColor( 0 )}}>관리 메뉴</Link> </div>
-                    <div className={"category " + ( color == 1 ? "active" : "" )} onClick={(e)=>{changeColor( 1 )}}> <Link to={"/reservation"}>객실 예약 관리</Link> </div>
-                    <div className={"category " + ( color == 2 ? "active" : "" )}  onClick={(e)=>{changeColor( 2 )}}> 시설 예약 관리 </div>
-                    <div className={"category " + ( color == 3 ? "active" : "" )}  onClick={(e)=>{changeColor( 3)}}><Link to={"/roomManagement"}>객실 관리</Link> </div>
-                    <div className={"category " + ( color == 4 ? "active" : "" )}  onClick={(e)=>{changeColor( 4 )}}> 시설 관리 </div>
-                    <div className={"category " + ( color == 5 ? "active" : "" )}  onClick={(e)=>{changeColor( 5 )}}> 직원 관리 </div>
-                    <div className={"category " + ( color == 6 ? "active" : "" )}  onClick={(e)=>{changeColor( 6 )}}> 매출 관리 </div>
+                    <Link to={"/reservation"} className={"category " + ( color == 1 ? "active" : "" )} onClick={(e)=>{changeColor( 1 )}}> 객실 예약 관리</Link>
+                    <Link to={"/"} className={"category " + ( color == 2 ? "active" : "" )}  onClick={(e)=>{changeColor( 2 )}}> 시설 예약 관리 </Link>
+                    <Link to={"/roomManagement"} className={"category " + ( color == 3 ? "active" : "" )}  onClick={(e)=>{changeColor( 3)}}>객실 관리</Link>
+                    <Link to={"/"} className={"category " + ( color == 4 ? "active" : "" )}  onClick={(e)=>{changeColor( 4 )}}> 시설 관리 </Link>
+                    <Link to={"/"} className={"category " + ( color == 5 ? "active" : "" )}  onClick={(e)=>{changeColor( 5 )}}> 직원 관리 </Link>
+                    <Link to={"/"} className={"category " + ( color == 6 ? "active" : "" )}  onClick={(e)=>{changeColor( 6 )}}> 매출 관리 </Link>
                 </div>
             </div>
         </>)
