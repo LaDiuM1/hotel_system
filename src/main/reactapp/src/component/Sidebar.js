b402931ecf63aed6cd772bd3542ed8840199c879
LaDiuM <wjddhrtjd123@gmail.com>
2023-11-11 00:21:00 +0900

    메뉴 이름 수정, 시설 이용 현황 수정

--- src/main/reactapp/src/component/Sidebar.js
+++ src/main/reactapp/src/component/Sidebar.js
@@ -13,12 +13,13 @@ export default function Sidebar(){
             <div className={"sidebar"}>
                 <div className={"categoryMenu"}>
                     <div className={"mainText"}> <Link to={"/"} onClick={(e)=>{changeColor( 0 )}}>관리 메뉴</Link> </div>
-                    <Link to={"/reservation"} className={"category " + ( color == 1 ? "active" : "" )} onClick={(e)=>{changeColor( 1 )}}> 객실 예약 관리</Link>
-                    <Link to={"/locationReservation"} className={"category " + ( color == 2 ? "active" : "" )}  onClick={(e)=>{changeColor( 2 )}}> 시설 예약 관리 </Link>
-                    <Link to={"/roomManagement"} className={"category " + ( color == 3 ? "active" : "" )}  onClick={(e)=>{changeColor( 3)}}>객실 관리</Link>
-                    <Link to={"/locationManagement"} className={"category " + ( color == 4 ? "active" : "" )}  onClick={(e)=>{changeColor( 4 )}}> 시설 관리 </Link>
+                    <Link to={"/reservation"} className={"category " + ( color == 1 ? "active" : "" )} onClick={(e)=>{changeColor( 1 )}}> 객실 예약 정보</Link>
+                    <Link to={"/locationReservation"} className={"category " + ( color == 2 ? "active" : "" )}  onClick={(e)=>{changeColor( 2 )}}> 시설 예약 정보 </Link>
+                    <Link to={"/roomManagement"} className={"category " + ( color == 3 ? "active" : "" )}  onClick={(e)=>{changeColor( 3)}}>객실 이용 현황</Link>
+                    <Link to={"/locationManagement"} className={"category " + ( color == 4 ? "active" : "" )}  onClick={(e)=>{changeColor( 4 )}}> 시설 이용 현황 </Link>
                     <Link to={"/"} className={"category " + ( color == 5 ? "active" : "" )}  onClick={(e)=>{changeColor( 5 )}}> 직원 관리 </Link>
                     <Link to={"/"} className={"category " + ( color == 6 ? "active" : "" )}  onClick={(e)=>{changeColor( 6 )}}> 매출 관리 </Link>
+                    <Link to={"/"} className={"category " + ( color == 7 ? "active" : "" )}  onClick={(e)=>{changeColor( 7 )}}> 운영 관리 </Link>
                 </div>
             </div>
         </>)
