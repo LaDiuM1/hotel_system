6efb05a0708cb08962e6b7885490c10238170983
kimsundae <naa159@naver.com>
2023-11-20 16:09:31 +0900

    16:08 마무리 작업, 시큐리티 적용하기, 권한 부여하기, 유효성 검사 적용하기(페이지 별 권한), 로그아웃 기능 추가

--- src/main/java/hotelManagement/service/Business/staticsService/TotalUserStatisticsService.java
+++ src/main/java/hotelManagement/service/Business/staticsService/TotalUserStatisticsService.java
@@ -2,18 +2,20 @@ package hotelManagement.service.business.staticsService;
 
 import hotelManagement.model.dto.business.OperationStatisticsDto;
 import hotelManagement.model.repository.business.statistics.TotalUserStatisticsRepositiory;
+import hotelManagement.service.util.ReportStatistics;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
 
 @Service
-public class TotalUserStatisticsService {
+public class TotalUserStatisticsService implements ReportStatistics {
     @Autowired
     private TotalUserStatisticsRepositiory totalUserStatisticsRepositiory;
     /*
     * 통계 가져오기
     * */
+    @Override
     public List<Map<String,Object>> reportStatistics(OperationStatisticsDto operationStatisticsDto ){
 
         final String category = operationStatisticsDto.getDateCategory();
@@ -34,6 +36,7 @@ public class TotalUserStatisticsService {
     /*
     * 카테고리에 따른 통계
     * */
+    @Override
     public List<Map<String,Object>> yearTotal(){
 
         List<Object[]> yearLocationTotalSales = totalUserStatisticsRepositiory.yearLocationTotal();
@@ -73,17 +76,26 @@ public class TotalUserStatisticsService {
                 put( "x", String.valueOf((int)objects[0])); put("y", objects[1]);
             }});
 
-
         return Arrays.asList( locationMap, roomresvMap, ticketMap );
     }
     /*
      * 카테고리에 따른 통계
      * */
+    @Override
     public List<Map<String,Object>> monthTotal( String year ){
 
         List<Object[]> monthLocationTotalSales = totalUserStatisticsRepositiory.monthLocationTotal(year);
         List<Object[]> monthRoomReservationTotalSales = totalUserStatisticsRepositiory.monthRoomTotal(year);
         List<Object[]> monthTicketTotalSales = totalUserStatisticsRepositiory.monthLocationTotal(year);
+
+        // 1월부터 12월까지 0으로 초기화 하기 위한 리스트
+        List<Map<String,Object>> hasMonth = new ArrayList<>();
+        // 1월부터 12월까지 hasMonth에 넣는 for문 각 개월은 0으로 초기화
+        for( int i = 1; i <= 12; i++ ){
+            HashMap<String, Object> map = new HashMap<>();
+            map.put( "x", i +"월"); map.put( "y" , 0);
+            hasMonth.add( map );
+        }
 
         Map<String,Object> locationMap = new HashMap<String,Object>(){{
             put("id","시설"); put("color","hsl(286, 70%, 50%)" );
@@ -124,43 +136,59 @@ public class TotalUserStatisticsService {
     /*
     * 주간 통계
     * */
+    @Override
     public List<Map<String,Object>> weekTotal( String year, String month ){
-        List<Object[]> weekLocationTotalSales = totalUserStatisticsRepositiory.weekLocationTotal(year,month);
-        List<Object[]> weekRoomReservationTotalSales = totalUserStatisticsRepositiory.weekRoomReservationTotal(year,month);
-        List<Object[]> weekTicketTotalSales = totalUserStatisticsRepositiory.weekTicketTotal(year,month);
+        List<Object[]> weekLocationTotalUser = totalUserStatisticsRepositiory.weekLocationTotal(year,month);
+        List<Object[]> weekRoomReservationTotalUser = totalUserStatisticsRepositiory.weekRoomReservationTotal(year,month);
+        List<Object[]> weekTicketTotalUser = totalUserStatisticsRepositiory.weekTicketTotal(year,month);
+
+        // 1주차부터 6주차까지 0으로 초기화 하기 위한 리스트
+        List<Map<String,Object>> hasWeek = new ArrayList<>();
+        // 1주차부터 6주차까지 hasWeek 넣는 for문 각 개월은 0으로 초기화
+        for( int i = 1; i <= 6; i++ ){
+            HashMap<String, Object> map = new HashMap<>();
+            map.put( "x", i +"주차"); map.put( "y" , 0);
+            hasWeek.add( map );
+        }
 
         Map<String,Object> locationMap = new HashMap<String,Object>(){{
             put("id","시설"); put("color","hsl(286, 70%, 50%)" );
-            put("data", new ArrayList<Map<String,Object>>() );
+            put("data", new ArrayList<Map<String,Object>>(hasWeek) );
         }};
         Map<String,Object> roomresvMap = new HashMap<String,Object>(){{
             put("id","객실"); put("color","hsl(339, 70%, 50%)" );
-            put("data", new ArrayList<Map<String,Object>>() );
+            put("data", new ArrayList<Map<String,Object>>(hasWeek) );
         }};
         Map<String,Object> ticketMap = new HashMap<String,Object>(){{
             put("id","회원권"); put("color","hsl(52, 70%, 50%)" );
-            put("data", new ArrayList<Map<String,Object>>() );
+            put("data", new ArrayList<Map<String,Object>>(hasWeek) );
         }};
 
         ArrayList<Map<String,Object>> maps;
 
         maps = ((ArrayList<Map<String,Object>>)locationMap.get("data"));
-        for( Object[] objects : weekLocationTotalSales )
-            maps.add( new HashMap<String,Object>(){{
-                put( "x", String.valueOf((int)objects[0])); put("y", objects[1]);
-            }});
+        for( Object[] objects : weekLocationTotalUser ){
+            int week = (int)objects[0]-1;
+            if( maps.get( week ).containsValue(0) ){
+                maps.get( week ).put("y",objects[1]);
+            }
+        }
 
         maps = ((ArrayList<Map<String,Object>>)roomresvMap.get("data"));
-        for( Object[] objects : weekRoomReservationTotalSales )
-            maps.add( new HashMap<String,Object>(){{
-                put( "x",String.valueOf((int)objects[0])); put("y", objects[1]);
-            }});
+        for( Object[] objects : weekRoomReservationTotalUser ){
+            int week = (int)objects[0]-1;
+            if( maps.get( week ).containsValue(0) ){
+                maps.get( week ).put("y",objects[1]);
+            }
+        }
 
         maps = ((ArrayList<Map<String,Object>>)ticketMap.get("data"));
-        for( Object[] objects : weekTicketTotalSales )
-            maps.add( new HashMap<String,Object>(){{
-                put( "x", String.valueOf((int)objects[0])); put("y", objects[1]);
-            }});
+        for( Object[] objects : weekTicketTotalUser ){
+            int week = (int)objects[0]-1;
+            if( maps.get( week ).containsValue(0) ){
+                maps.get( week ).put("y",objects[1]);
+            }
+        }
 
 
         return Arrays.asList( locationMap, roomresvMap, ticketMap );
