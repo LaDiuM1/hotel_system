e04fd6f445d93ab9a6e4be325713ccbcee5eb025
kimsundae <naa159@naver.com>
2023-11-21 14:43:52 +0900

    14:37 객실 코드 재 수정

--- src/main/reactapp/src/component/room/RoomReservation.js
+++ src/main/reactapp/src/component/room/RoomReservation.js
@@ -108,10 +108,10 @@ export default function RoomReservation(){
                                 let tableColumn = [
                                     {className:"guestRoomNum" ,ctitle:"호실",cname:"rrno"},
                                     {className:"guestRoomGrade" ,ctitle:"등급",cname:"rgrade"},
-                                    {className:"guestRoomName" ,ctitle:"성함",cname:"rname"},
-                                    {className:"guestRoomPhone" ,ctitle:"전화번호",cname:"rphone"},
                                     {className:"guestRoomCheckIn" ,ctitle:"체크인",cname:"rcin"},
                                     {className:"guestRoomCheckOut" ,ctitle:"체크아웃",cname:"rcout"},
+                                    {className:"guestRoomName" ,ctitle:"성함",cname:"rname"},
+                                    {className:"guestRoomPhone" ,ctitle:"전화번호",cname:"rphone"},
                                     {className:"guestRoomStart" ,ctitle:"시작 날짜",cname:"sdate"},
                                     {className:"guestRoomEnd" ,ctitle:"종료 날짜",cname:"edate"},
                                 ]
